a6d52f04e0df1304199ba5013cefe4a411fed068
olfis <olfis@o2.pl>
2022-11-10 16:20:00 +0100

    web tests

---
+++ main.py
@@ -0,0 +1,30 @@
+import os
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+os.environ['PATH'] += r'C:/Selenium_driver'
+driver = webdriver.Chrome()
+
+'''driver.get('https://demo.seleniumeasy.com/jquery-download-progress-bar-demo.html')
+driver.implicitly_wait(8)
+element = driver.find_element(By.ID, 'downloadButton')
+element.click()
+
+WebDriverWait(driver, 30).until(
+    EC.text_to_be_present_in_element(
+        (By.CLASS_NAME, 'progress-label'), 'Complete!'
+    )
+)'''
+
+driver.get('https://demo.seleniumeasy.com/basic-first-form-demo.html')
+driver.implicitly_wait(6)
+num1 = driver.find_element(By.ID, 'sum1')
+num2 = driver.find_element(By.ID, 'sum2')
+
+num1.send_keys(Keys.NUMPAD4, Keys.NUMPAD5)
+num2.send_keys(Keys.NUMPAD6, Keys.NUMPAD9)
+
